3b1895b11b7d712e2599e0999818fc70f696211d
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-07-27 13:50:11 +0300

    configures custom color pallete in tailwind

--- src/components/profile-page/profile-feed.js
+++ src/components/profile-page/profile-feed.js
@@ -1,32 +1,32 @@
-import React from "react";
-import profileImg from "./imgs/profile.jpg";
+import React from 'react';
+import profileImg from './imgs/profile.jpg';
 
 const ProfileFeed = () => {
   return (
-    <nav className="flex justify-around items-center w-screen py-6 bg-paletteDark text-paletteBeige text-sm">
+    <nav className="flex justify-around items-center w-screen py-6 bg-darkP text-beige text-sm">
       <h1 className="text-base">
-        <a href={"#"}>We Care</a>
+        <a href={'#'}>We Care</a>
       </h1>
       <ul className="flex">
         <li className="mr-10">
-          <a href={"#"}>For you</a>
+          <a href={'#'}>For you</a>
         </li>
         <li className="mr-10">
-          <a href={"#"}>Counseling</a>
+          <a href={'#'}>Counseling</a>
         </li>
         <li>
-          <a href={"#"}>Connect Now</a>
+          <a href={'#'}>Connect Now</a>
         </li>
       </ul>
       <div className="flex items-center">
-        <a href={"#"}>
+        <a href={'#'}>
           <img
             src={profileImg}
             className="rounded-full h-10 w-10 object-cover"
           ></img>
         </a>
         <p className="ml-2">
-          <a href={"#"}>Bruce Lee</a>
+          <a href={'#'}>Bruce Lee</a>
         </p>
       </div>
     </nav>
