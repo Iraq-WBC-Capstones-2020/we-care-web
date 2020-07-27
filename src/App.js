b85ce8196fd36664e8743e92f1f0b764112ea658
Yara Alkassar <yara.sarmad@yahoo.com>
2020-07-28 00:38:22 +0300

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div>
-      <h1>We Care</h1>
+      <Navbar />
+      {/* <h1>We Care</h1> */}
     </div>
   );
 }
