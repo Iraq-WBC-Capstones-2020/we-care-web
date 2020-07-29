66896e4e424cfb717c9be292845253d4234b5ba1
Yara Alkassar <yara.sarmad@yahoo.com>
2020-07-29 14:50:17 +0300

    Added content

--- src/components/Counseling SignUp/Sign Up Page.js
+++ src/components/Counseling SignUp/Sign Up Page.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Form from './Form';
+import Content from './Content';
 const CounslerSignUp = () => {
   return (
     <>
       <div className="bg-darkBeige h-full w-full ">
         <Navbar />
-        <div
-          className="flex justify-around items-center w-screen h-screen 
-        py-6"
-        >
+        <div className="flex justify-around items-center h-screen">
           {/* this div is for splitting the other two*/}
-
-          <Form />
+          <Form></Form>
+          <Content />
         </div>
       </div>
     </>
