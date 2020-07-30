c6cc389108bdf1e57b16ccdd262e6af76aae1119
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-07-30 12:02:06 +0300

    creates the profile section

--- src/components/profile-page/profile/right-sidebar.js
+++ src/components/profile-page/profile/right-sidebar.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import profilePic from '../imgs/profile.png';
+import { FiSmile } from 'react-icons/fi';
 
 const RightSidebar = () => {
   return (
     <div className="w-1/5 h-full lg:w-2/12 bg-darkP flex flex-col justify-center text-beige">
       <div className="w-full self-center flex flex-col items-center">
-        <button className="text-orangeP border border-orangeP border-solid rounded px-4 py-2">
+        <div className="flex mb-3">
+          <div className="mr-3">
+            <img className="rounded-full w-12" src={profilePic} />
+          </div>
+          <div>
+            <h2 className="text-lg font-semibold">Charles Davies</h2>
+            <p className="text-sm font-light">Joined three years ago</p>
+          </div>
+        </div>
+        <div className="border border-beige border-solid rounded-full py-2 mt-10 w-32 text-sm flex items-center justify-center">
+          <FiSmile className="mr-3 text-xl" /> 34 friends
+        </div>
+        <button className="text-orangeP border border-orangeP border-solid rounded py-2 mt-10 w-32 text-sm">
           Change Avatar
+        </button>
+        <button className="text-orangeP border border-orangeP border-solid rounded py-2 mt-6 w-32 text-sm">
+          Edit About
         </button>
       </div>
     </div>
