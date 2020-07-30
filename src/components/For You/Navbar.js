4678576f0009772d258e60db35bcbbf3ed203665
Yara Alkassar <yara.sarmad@yahoo.com>
2020-07-30 10:06:55 +0300

    Changed Navbar naming

---
+++ src/components/For You/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+const Navbar = () => {
+  return (
+    <>
+      <nav className="flex justify-around items-center py-6 bg-darkBeige text-darkP text-sm">
+        <h1 className="text-lg">
+          <a href={'#'} style={{ fontFamily: 'Pacifico' }}>
+            We Care
+          </a>
+        </h1>
+        <ul className="flex">
+          <li className="mr-10">
+            <a href={'#'}>For you</a>
+          </li>
+          <li className="mr-10">
+            <a href={'#'}>Counseling</a>
+          </li>
+          <li className="mr-10">
+            <a href={'#'}>Sign Up</a>
+          </li>
+          <li className="mr-10">
+            <a href={'#'}>Log In</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
