b4eac2d504bdbd203e5151d606a9107f83c54f54
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-07-31 09:07:39 +0300

    refactors chatroom page

---
+++ src/components/chatroom-page/chat-box.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Message from './message';
+import PropTypes from 'prop-types';
+import MessageTextarea from './message-textarea';
+import { IoIosCloseCircle } from 'react-icons/io';
+
+const Messages = ({ messages, setMessages }) => {
+  return (
+    <div
+      className="flex flex-col justify-between w-7/12 bg-beige m-5 rounded-lg relative overflow-y-auto overflow-x-hidden"
+      style={{ height: '90%' }}
+    >
+      <button className="focus:outline-none sticky top-0 ">
+        <IoIosCloseCircle className="text-darkP text-2xl" />
+      </button>
+      <div className="w-11/12 self-center flex flex-col">
+        {messages.map((message, index) =>
+          message.from === 'you' ? (
+            <Message message={message} classes={'self-end'} key={index} />
+          ) : (
+            <Message message={message} classes={'self-start'} key={index} />
+          )
+        )}
+      </div>
+      <MessageTextarea messages={messages} setMessages={setMessages} />
+    </div>
+  );
+};
+
+export default Messages;
+
+Messages.propTypes = {
+  messages: PropTypes.array.isRequired,
+  setMessages: PropTypes.func.isRequired,
+};
--- src/components/chatroom-page/chatroom-page.js
+++ src/components/chatroom-page/chatroom-page.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
 import ProfileNavbar from './navbar';
 import illustration from './imgs/chat-undraw.svg';
-import { IoIosCloseCircle } from 'react-icons/io';
-import Messages from './messages';
-import MessageTextarea from './message-textarea';
+import Messages from './chat-box';
 import './scrollBar.css';
 
 const ChatroomPage = () => {
@@ -37,18 +35,7 @@ const ChatroomPage = () => {
       <ProfileNavbar />
       <main className="flex justify-evenly items-center h-full w-screen overflow-hidden">
         <img src={illustration} className="w-1/4" />
-        <div
-          className="flex flex-col justify-between w-7/12 bg-beige m-5 rounded-lg relative overflow-y-auto overflow-x-hidden"
-          style={{ height: '90%' }}
-        >
-          <button className="focus:outline-none sticky top-0 ">
-            <IoIosCloseCircle className="text-darkP text-2xl" />
-          </button>
-          <div className="w-11/12 self-center flex flex-col">
-            <Messages messages={messages} />
-          </div>
-          <MessageTextarea messages={messages} setMessages={setMessages} />
-        </div>
+        <Messages messages={messages} setMessages={setMessages} />
       </main>
     </div>
   );
