0fdab1fb4dea79d4f88a4ca11e1ef941b3d05970
Yara Alkassar <yara.sarmad@yahoo.com>
2020-08-01 23:47:30 +0300

    fix: changed logo to image

--- src/components/Counseling SignUp/Navbar.js
+++ src/components/Counseling SignUp/Navbar.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import logo from '../Images/Logo.svg';
+
 const GeneralNavbar = () => {
   return (
     <>
       <nav className="flex justify-around items-center py-6 bg-darkBeige text-darkP text-sm">
-        <h1 className="text-lg">
-          <a href={'#'} style={{ fontFamily: 'Pacifico' }}>
-            We Care
-          </a>
-        </h1>
+        <img src={logo} className="w-20" alt="We Care logo" />
+
         <ul className="flex">
           <li className="mr-10">
             <a href={'#'}>For you</a>
--- src/components/For You/Navbar.js
+++ src/components/For You/Navbar.js
@@ -1,30 +1,30 @@
 import React from 'react';
-const Navbar = () => {
+import profileImg from '../Images/Profile.png';
+import logo from '../Images/Logo.svg';
+
+const ProfileNavbar = () => {
   return (
-    <>
-      <nav className="flex justify-around items-center py-6 bg-darkBeige text-darkP text-sm">
-        <h1 className="text-lg">
-          <a href={'#'} style={{ fontFamily: 'Pacifico' }}>
-            We Care
-          </a>
-        </h1>
-        <ul className="flex">
-          <li className="mr-10">
-            <a href={'#'}>For you</a>
-          </li>
-          <li className="mr-10">
-            <a href={'#'}>Counseling</a>
-          </li>
-          <li className="mr-10">
-            <a href={'#'}>Sign Up</a>
-          </li>
-          <li className="mr-10">
-            <a href={'#'}>Log In</a>
-          </li>
-        </ul>
-      </nav>
-    </>
+    <nav className="flex justify-around items-center w-screen py-6 text-darkP text-sm">
+      <img src={logo} className="w-20" alt="We Care logo" />
+      <ul className="flex">
+        <li className="mr-10">
+          <a href={'#'}>Home</a>
+        </li>
+        <li className="mr-10">
+          <a href={'#'}>For you</a>
+        </li>
+        <li>
+          <a href={'#'}>Counseling</a>
+        </li>
+      </ul>
+      <div className="flex justify-center items-center">
+        <img
+          src={profileImg}
+          className="rounded-full h-10 w-10 object-cover"
+        ></img>
+        <p className="ml-2">Bruce Lee</p>
+      </div>
+    </nav>
   );
 };
-
-export default Navbar;
+export default ProfileNavbar;
