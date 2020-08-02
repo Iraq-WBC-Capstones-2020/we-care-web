2761729829a3567dc9df226dc5b4aeefa9e91b37
Yara Alkassar <yara.sarmad@yahoo.com>
2020-08-02 05:00:35 +0300

    Improvement: Style

--- src/components/For You/Front Content.js
+++ src/components/For You/Front Content.js
@@ -3,24 +3,30 @@ import connection from '../Images/undraw_connection_b38q.svg';
 const ForYouContent = () => {
   return (
     <>
-      <div className="bg-beige flex justify-between">
-        <div
-          style={{ fontFamily: 'Roboto' }}
-          className="text-darkP ml-32 mr-auto w-1/2 md:mt-20"
-        >
-          <h1 className="text-base md:text-3xl leading-relaxed font-semibold ">
-            For You
-          </h1>
-          <h6 className="font-medium text-sm sm:text-base md:text-xl leading-relaxed md:w-2/3 lg:w-1/3">
-            Your motivational area, articles from the team and the community.
-          </h6>
+      <section className="text-darkP bg-beige">
+        <div className="container px-5 py-24 mx-auto flex flex-wrap">
+          <div className="flex flex-wrap my-auto lg:w-1/2 sm:w-2/3 content-start sm:pr-10">
+            <div className="w-full">
+              <h1 className="mb-2 text-base md:text-3xl leading-relaxed font-semibold ">
+                For You
+              </h1>
+            </div>
+            <div className="md:w-2/3 ">
+              <h2 className="font-medium text-sm sm:text-base md:text-xl">
+                Your motivational area, articles from the team and the
+                community.
+              </h2>
+            </div>
+          </div>
+          <div className="lg:w-1/2 sm:w-1/3 w-full mt-6 sm:mt-0">
+            <img
+              src={connection}
+              alt="connection"
+              className=" object-right w-full h-full"
+            />
+          </div>
         </div>
-        <img
-          src={connection}
-          alt="connection"
-          className="w-1/3 ml-auto mr-6 md:mt-20"
-        />
-      </div>
+      </section>
     </>
   );
 };
