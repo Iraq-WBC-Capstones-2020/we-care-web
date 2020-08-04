a96fa57b97d443c1fa68af4e32a3d1fb5ee909c8
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-04 21:35:12 +0300

    does the responsiveness of counselor sign up page. halfway there

--- src/components/Counseling SignUp/Content.js
+++ src/components/Counseling SignUp/Content.js
@@ -2,21 +2,19 @@ import React from 'react';
 import plant from '../Images/Plant 05.svg';
 const Content = () => {
   return (
-    <>
-      <div className="w-1/2 sm:w-auto md:w-1/3 lg:w-1/3 xl:w-1/3 md:flex flex-col mx-10 mt-4">
-        <div style={{ fontFamily: 'Roboto' }} className="mt-auto">
-          <h1 className="text-2xl lg:text-5xl text-white leading-relaxed font-semibold ">
-            Be A Reason.
-          </h1>
-          <h6 className="text-darkP font-medium text-xl lg:text-2xl">
-            A reason someone is smiling and alive.
-          </h6>
-        </div>
-        <div className="hidden md:block mr-20 ml-auto mt-10">
-          <img src={plant} alt="Plant"></img>
-        </div>
+    <div className="order-1 lg:order-2 w-full flex flex-col justify-between items-center">
+      <div className="w-full pl-32 lg:pl-0">
+        <h1 className="lg:text-5xl text-6xl text-white leading-relaxed font-semibold">
+          Be A Reason
+        </h1>
+        <h6 className="text-darkP lg:text-3xl text-4xl font-light lg:pr-40 pr-64">
+          A reason someone is smiling and alive.
+        </h6>
       </div>
-    </>
+      <div className="mt-10 lg:w-auto w-full lg:block flex justify-end">
+        <img className="lg:w-4/5 w-48" src={plant} alt="Plant"></img>
+      </div>
+    </div>
   );
 };
 
