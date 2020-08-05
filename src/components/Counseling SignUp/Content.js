91a69038b6d25c41b5f3de2d6074f4e4c1024595
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-05 07:14:15 +0300

    makes counselor sign-up page responsive, fixes #18

--- src/components/Counseling SignUp/Content.js
+++ src/components/Counseling SignUp/Content.js
@@ -2,17 +2,17 @@ import React from 'react';
 import plant from '../Images/Plant 05.svg';
 const Content = () => {
   return (
-    <div className="order-1 lg:order-2 w-full flex flex-col justify-between items-center">
-      <div className="w-full pl-32 lg:pl-0">
-        <h1 className="lg:text-5xl text-6xl text-white leading-relaxed font-semibold">
+    <div className="order-1 xl:order-2 md:w-auto w-full flex flex-col xl:justify-between justify-center items-end xl:mb-auto mb-8">
+      <div className="w-full flex flex-col md:pl-0 pl-12">
+        <h1 className="md:text-5xl text-4xl text-white leading-relaxed font-semibold">
           Be A Reason
         </h1>
-        <h6 className="text-darkP lg:text-3xl text-4xl font-light lg:pr-40 pr-64">
+        <h6 className="text-darkP md:text-3xl text-2xl font-light md:w-3/5 w-11/12">
           A reason someone is smiling and alive.
         </h6>
       </div>
-      <div className="mt-10 lg:w-auto w-full lg:block flex justify-end">
-        <img className="lg:w-4/5 w-48" src={plant} alt="Plant"></img>
+      <div className="mt-10 lg:block flex md:justify-end justify-center md:w-auto w-full">
+        <img className="md:w-9/12 w-5/12" src={plant} alt="Plant"></img>
       </div>
     </div>
   );
