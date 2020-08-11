e8bc725d2cbf894db70b11762fef5e17476ecbc4
Yara <yaraalkassar@users.noreply.github.com>
2020-08-11 13:57:26 +0300

    solved key problem

--- src/components/For You/Quotes.js
+++ src/components/For You/Quotes.js
@@ -55,7 +55,7 @@ const Quotes = () => {
                 // eslint-disable-next-line react/jsx-key
                 return (
                   <>
-                    <div className="p-4 md:w-1/3">
+                    <div className="p-4 md:w-1/3" key={index}>
                       <div className="flex rounded-lg h-full bg-white p-8 flex-col">
                         <div className="flex items-center mb-3 pb-4 border-b border-darkP">
                           <h2 className=" text-lg title-font font-medium">
