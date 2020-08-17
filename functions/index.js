d8fc1ce936ce313e3d24a199f3b580b766d9151c
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-17 16:07:36 +0300

    writes cloud function to synchronize real-time db with firestore

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,23 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// const admin = require('firebase-admin');
-// admin.initializeApp();
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const db = admin.firestore();
+
+exports.onUserStatusChanged = functions.database
+  .ref('status/{userId}')
+  .onUpdate((change, context) => {
+    const eventStatus = change.after.val();
+    const userDoc = db.doc(`users/${context.params.userId}`);
+
+    return change.after.ref.once('value').then((snapshot) => {
+      const status = snapshot.val();
+      if (status.lastChanged > eventStatus.lastChanged) {
+        return;
+      }
+
+      eventStatus.lastChanged = new Date(eventStatus.lastChanged);
+      userDoc.update({ status: eventStatus });
+    });
+  });
