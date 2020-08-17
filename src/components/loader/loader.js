3f908573266dffb1317946187fa64dc6cd8c2211
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-18 00:57:36 +0300

    changes size & color of the loader

--- src/components/loader/loader.js
+++ src/components/loader/loader.js
@@ -3,7 +3,7 @@ import './loader.css';
 
 const Loader = () => {
   return (
-    <div className="h-screen w-screen bg-darkP">
+    <div className="h-screen w-screen bg-beige flex justify-center items-center">
       <div className="sk-chase">
         <div className="sk-chase-dot"></div>
         <div className="sk-chase-dot"></div>
