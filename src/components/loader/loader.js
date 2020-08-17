af4f39654c4d9ad78d050d7614a4e1f556366d85
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-18 00:48:26 +0300

    adds the loader and its styles

--- src/components/loader/loader.js
+++ src/components/loader/loader.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import './loader.css';
 
 const Loader = () => {
-  return <div className="h-screen w-screen bg-darkP">Loading...</div>;
+  return (
+    <div className="h-screen w-screen bg-darkP">
+      <div className="sk-chase">
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Loader;
