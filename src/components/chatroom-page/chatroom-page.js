05d501bb4f0453fd3b5d55b14a27240777aafb69
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-18 18:54:54 +0300

    implements creating a chatroom document and stores both user's Ids in it

--- src/components/chatroom-page/chatroom-page.js
+++ src/components/chatroom-page/chatroom-page.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from './navbar';
 import illustration from './imgs/chat-undraw.svg';
-import Messages from './chat-box';
+// import Messages from './chat-box';
 import './scrollBar.css';
 import firebase from './../../firebase/firebase';
 import { useSelector } from 'react-redux';
 
 const ChatroomPage = () => {
   const currentUser = useSelector((state) => state.currentUser);
+  const [matchedUser, setMatchedUser] = useState(null);
+  const [foundMatch, setFoundMatch] = useState(false);
+  const [chatroomId, setChatroomId] = useState(null);
+  // const [messages, setMessages] = useState([]);
 
   async function findWaitingUsers() {
     const usersRef = firebase.db.collection('users');
@@ -17,16 +21,56 @@ const ChatroomPage = () => {
     } else {
       queryRef = usersRef.where('role', '==', 'listener');
     }
-    const allTherapistsRes = await queryRef.get();
-    allTherapistsRes.forEach((therapist) => console.log(therapist.data()));
-    // console.log(allTherapistsRes);
+    const allUserRes = await queryRef.get();
+    allUserRes.forEach((user) => setMatchedUser(user.data())); // TODO: limit down to one user based on waiting time
+  }
+
+  async function createAChatroomDoc() {
+    return await firebase.db
+      .collection('chatrooms')
+      .add({
+        listenerId:
+          currentUser.role === 'listener' ? currentUser.uid : matchedUser.uid,
+        memberId:
+          currentUser.role === 'member' ? currentUser.uid : matchedUser.uid,
+      })
+      .then((doc) => setChatroomId(doc.id));
   }
 
   useEffect(() => {
-    if (currentUser) {
+    if (currentUser && foundMatch === false) {
       findWaitingUsers();
+      setFoundMatch(true);
+      console.log('hey');
     }
   }, [currentUser]);
+
+  useEffect(() => {
+    if (currentUser && matchedUser) {
+      console.log('hiiii');
+      createAChatroomDoc();
+    }
+  }, [matchedUser]);
+
+  // TODO: store the matched user's IDs in the chatroom document istead of storing the chatroomId in the user's docs
+
+  // async function getMessagesFromChatroomDoc() {
+  //   // TODO: get all the messages & store it in messages array
+  //   await firebase.db
+  //     .collection('chatrooms')
+  //     .doc(chatroomId)
+  //     .onSnapshot((snapshot) => {
+  //       console.log(snapshot);
+  //     });
+  // }
+
+  // async function addMessagesToChatroomDoc() {
+  //   // TODO: add one document for each message. maybe in a sub collection
+  //   await firebase.db
+  //     .collection(`chatrooms/${chatroomId}`)
+  //     .doc(chatroomId)
+  //     .set({});
+  // }
 
   // async function findAvailableUsersRTDB() {
   //   const allUserStatuses = await firebase.rtdb
@@ -41,31 +85,7 @@ const ChatroomPage = () => {
   //     });
   // }
   // currentUser && findAvailableUsersRTDB()
-  // const [messages, setMessages] = useState([
-  //   {
-  //     nickname: 'wonderful-sky',
-  //     from: 'them',
-  //     to: 'you',
-  //     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  //     createdAt: '2:45 PM',
-  //   },
-  //   {
-  //     nickname: 'Kind-heart',
-  //     from: 'you',
-  //     to: 'them',
-  //     body:
-  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet.',
-  //     createdAt: '2:47 PM',
-  //   },
-  //   {
-  //     nickname: 'wonderful-sky',
-  //     from: 'them',
-  //     to: 'you',
-  //     body:
-  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet.',
-  //     createdAt: '2:50 PM',
-  //   },
-  // ]);
+
   return (
     <div className="h-screen flex flex-col bg-darkP">
       <Navbar />
