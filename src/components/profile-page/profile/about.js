50da4895ffdf5ecefb388dc65dd2c576ec42dbbf
zainab azzam <zainab_azzam@yahoo.com>
2020-08-20 20:39:08 +0300

    about page

--- src/components/profile-page/profile/about.js
+++ src/components/profile-page/profile/about.js
@@ -1,26 +1,126 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import Firebase from '../../../firebase/firebase';
 const ProfileAbout = () => {
+  const [like, setLike] = useState('');
+  const [dislike, setDisLike] = useState('');
+  const [Song, setSong] = useState('');
+  const [book, setBook] = useState('');
+  const [editMode, setEditMode] = useState(false);
+  const changeToFalse = () => {
+    setEditMode(false);
+  };
+  const changeToTrue = () => {
+    setEditMode(true);
+  };
+  const currentUser = useSelector((state) => state.currentUser);
+  async function addAbout() {
+    const userDoc = Firebase.db.doc(`/users/${currentUser.uid}`);
+
+    changeToFalse();
+    userDoc.update({
+      about: {
+        likes: { like },
+        dislikes: dislike,
+        favouriteMovies: book,
+        favouriteSongs: Song,
+      },
+    });
+  }
   return (
-    <div className="bg-white w-11/12 md:w-3/5 lg:w-1/2 xl:w-5/12 rounded-md text-darkP flex flex-col p-8 text-center">
-      <h1 className="font-semibold text-2xl">About</h1>
-      <div className="my-4">
-        <h2 className="font-bold">Likes</h2>
-        <p className="md:text-base text-sm">Friendliness, Ice Creams</p>
+    currentUser && (
+      <div className="bg-white w-11/12 md:w-3/5 lg:w-1/2 xl:w-5/12 rounded-md text-darkP flex flex-col p-8 text-center">
+        {editMode ? (
+          <div>
+            <div>
+              <h1 className="font-semibold text-2xl">About</h1>
+              <div className="my-4">
+                <h2 className="font-bold">Likes</h2>
+                <input
+                  className="text-orangeP border lg:border-orangeP border-darkP lg:bg-transparent bg-darkP border-solid rounded px-10 h-10 text-sm"
+                  type="text"
+                  value={like}
+                  onChange={(e) => setLike(e.target.value)}
+                />
+              </div>
+              <div className="my-4">
+                <h2 className="font-bold">Dislikes</h2>
+                <input
+                  value={dislike}
+                  onChange={(e) => setDisLike(e.target.value)}
+                  className="text-orangeP border lg:border-orangeP border-darkP lg:bg-transparent bg-darkP border-solid rounded px-10 h-10 text-sm"
+                />
+              </div>
+              <div className="my-4">
+                <h2 className="font-bold">Favourite Songs</h2>
+                <input
+                  value={Song}
+                  onChange={(e) => setSong(e.target.value)}
+                  className="text-orangeP border lg:border-orangeP border-darkP lg:bg-transparent bg-darkP border-solid rounded px-10 h-10 text-sm"
+                />
+              </div>
+              <div className="my-4">
+                <h2 className="font-bold">Favourite Books</h2>
+                <input
+                  value={book}
+                  onChange={(e) => setBook(e.target.value)}
+                  className="text-orangeP border lg:border-orangeP border-darkP lg:bg-transparent bg-darkP border-solid rounded px-10 h-10 text-sm"
+                />
+              </div>
+              <row>
+                <button
+                  className="mr-2 text-orangeP border lg:border-orangeP border-darkP lg:bg-transparent bg-darkP border-solid rounded py-2 mt-6 w-32 text-sm"
+                  onClick={changeToFalse}
+                >
+                  Cancel
+                </button>
+
+                <button
+                  className="ml-2 text-orangeP border lg:border-orangeP border-darkP lg:bg-transparent bg-darkP border-solid rounded py-2 mt-6 w-32 text-sm"
+                  onClick={() => addAbout()}
+                >
+                  Submit
+                </button>
+              </row>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <h1 className="font-semibold text-2xl">About</h1>
+            <div className="my-4">
+              <h2 className="font-bold">Likes</h2>
+              <p className="md:text-base text-sm">
+                {currentUser.about.likes.like}
+              </p>
+            </div>
+            <div className="my-4">
+              <h2 className="font-bold">Dislikes</h2>
+              <p className="md:text-base text-sm">
+                {currentUser.about.dislikes}
+              </p>
+            </div>
+            <div className="my-4">
+              <h2 className="font-bold">Favourite Songs</h2>
+              <p className="md:text-base text-sm">
+                {currentUser.about.favouriteSongs}
+              </p>
+            </div>
+            <div className="my-4">
+              <h2 className="font-bold">Favourite Books</h2>
+              <p className="md:text-base text-sm">
+                {currentUser.about.favouriteMovies}
+              </p>
+            </div>
+            <button
+              className="text-orangeP border lg:border-orangeP border-darkP lg:bg-transparent bg-darkP border-solid rounded py-2 mt-6 w-32 text-sm"
+              onClick={changeToTrue}
+            >
+              Edit About
+            </button>
+          </div>
+        )}
       </div>
-      <div className="my-4">
-        <h2 className="font-bold">Dislikes</h2>
-        <p className="md:text-base text-sm">Horror movies</p>
-      </div>
-      <div className="my-4">
-        <h2 className="font-bold">Favourite Songs</h2>
-        <p className="md:text-base text-sm">Somewhere over the rainbow</p>
-      </div>
-      <div className="my-4">
-        <h2 className="font-bold">Favourite Books</h2>
-        <p className="md:text-base text-sm">You are a badass</p>
-      </div>
-    </div>
+    )
   );
 };
 
--- src/components/profile-page/profile/right-sidebar.js
+++ src/components/profile-page/profile/right-sidebar.js
@@ -1,18 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiSmile } from 'react-icons/fi';
 import { useSelector } from 'react-redux';
-
+import firebase from '../../../firebase/firebase';
 const RightSidebar = () => {
+  const allInputs = { imgUrl: '' };
+  const [imageAsFile, setImageAsFile] = useState('');
+  const [imageAsUrl, setImageAsUrl] = useState(allInputs);
   const currentUser = useSelector((state) => state.currentUser);
-
+  const handleImageAsFile = (e) => {
+    const image = e.target.files[0];
+    setImageAsFile(() => image);
+  };
+  const handleFireBaseUpload = (e) => {
+    e.preventDefault();
+    if (imageAsFile === '') {
+      alert('Choose a Valid Image to Submit');
+    }
+    const uploadTask = firebase.storage
+      .ref(`/images/${imageAsFile.name}`)
+      .put(imageAsFile);
+    uploadTask.on(
+      'state_changed',
+      (snapShot) => {
+        //takes a snap shot of the process as it is happening
+        console.log(snapShot);
+      },
+      (err) => {
+        //catches the errors
+        console.log(err);
+      },
+      () => {
+        // gets the functions from storage refences the image storage in firebase by the children
+        // gets the download url then sets the image from firebase as the value for the imgUrl key:
+        firebase.storage
+          .ref('images')
+          .child(imageAsFile.name)
+          .getDownloadURL()
+          .then((fireBaseUrl) => {
+            setImageAsUrl((prevObject) => ({
+              ...prevObject,
+              imgUrl: fireBaseUrl,
+            }));
+          });
+      }
+    );
+  };
   return (
     currentUser && (
       <div className="lg:w-1/5 order-1 lg:order-2 w-11/12 md:w-3/5 lg:h-full lg:mt-0 mt-10 mb-5 lg:mb-0 lg:p-0 p-10 lg:bg-darkP bg-white flex flex-col justify-center lg:text-beige text-darkP lg:rounded-none rounded">
         <div className="w-full h-auto self-center justify-evenly flex flex-col items-center">
           <div className="flex flex-col justify-center items-center">
             <img
-              className="rounded-full w-24 mb-4"
-              src={currentUser.profilePicture}
+              className="rounded-full w-24 h-24 mb-4"
+              src={imageAsUrl.imgUrl}
               alt="profile picture"
             />
             <h2 className="text-lg font-semibold lg:ml-0 ml-4">
@@ -26,10 +66,18 @@ const RightSidebar = () => {
             <FiSmile className="mr-3 text-xl" /> 34 friends
           </div>
           <button className="text-orangeP border lg:border-orangeP border-darkP lg:bg-transparent bg-darkP border-solid rounded py-2 mt-10 w-32 text-sm">
-            Change Avatar
+            <input
+              className=" cursor-pointer absolute block opacity-0 pin-r pin-t w-10"
+              type="file"
+              onChange={handleImageAsFile}
+            />
+            Change Image
           </button>
-          <button className="text-orangeP border lg:border-orangeP border-darkP lg:bg-transparent bg-darkP border-solid rounded py-2 mt-6 w-32 text-sm">
-            Edit About
+          <button
+            className="text-orangeP   border lg:border-orangeP border-darkP lg:bg-transparent bg-darkP border-solid rounded py-2 mt-6 w-32 text-sm"
+            onClick={handleFireBaseUpload}
+          >
+            Submit Change
           </button>
         </div>
       </div>
