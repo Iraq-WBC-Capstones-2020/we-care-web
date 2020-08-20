361f43703eb9873197bd3f40a7f0210a3cbe8d4b
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-20 16:09:48 +0300

    removes member from members field in real-time db once chatroom created

--- src/components/chatroom-page/chat-box.js
+++ src/components/chatroom-page/chat-box.js
@@ -33,6 +33,13 @@ const Messages = () => {
       });
   }, []);
 
+  if (
+    firebase.chatroomObj &&
+    firebase.chatroomObj.memberId === firebase.auth.currentUser.uid
+  ) {
+    firebase.rtdb.ref(`/members/${firebase.auth.currentUser.uid}`).remove();
+  }
+
   return (
     <div
       className="flex flex-col justify-between md:w-10/12 w-11/12 lg:w-7/12 bg-beige m-5 rounded-lg relative overflow-y-auto overflow-x-hidden"
