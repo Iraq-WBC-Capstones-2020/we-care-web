8ad928c603904b614cb0ef126cd20f69b188b067
Yara <yaraalkassar@users.noreply.github.com>
2020-08-21 01:20:21 +0300

    fix: a tags

--- src/components/For You/ForYou-Posts.js
+++ src/components/For You/ForYou-Posts.js
@@ -20,9 +20,9 @@ const ProfileFeed = () => {
     <>
       <div className="bg-darkP w-full h-full flex flex-col justify-between items-center overflow-y-auto">
         <div className="flex my-4 items-center justify-evenly">
-          <a className="text-orangeP border-b-2 border-orangeP py-2 md:text-lg  md:px-16 text-xs">
+          <p className="text-orangeP border-b-2 border-orangeP py-2 md:text-lg  md:px-16 text-xs">
             By The Community
-          </a>
+          </p>
         </div>
         <WritePostTextarea posts={posts} setPosts={setPosts} />
         <div className="w-full flex flex-col justify-center items-center ">
