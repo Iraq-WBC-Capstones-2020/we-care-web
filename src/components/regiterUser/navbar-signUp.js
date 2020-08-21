525b15d117509649fb5a7deb01aededb002527b1
Yara <yaraalkassar@users.noreply.github.com>
2020-08-21 20:02:12 +0300

    fix: remove counselling from Navbar.

--- src/components/regiterUser/navbar-signUp.js
+++ src/components/regiterUser/navbar-signUp.js
@@ -33,9 +33,6 @@ export default function Navbar() {
             <li className="lg:mr-10 lg:mt-0 lg:py-0 py-3 hover:text-orangeP lg:border-0 border-b border-beige">
               <Link to="/ForYou">For you</Link>
             </li>
-            <li className="lg:mr-10 lg:my-0 lg:py-0 py-3 hover:text-orangeP lg:border-0 border-b border-beige">
-              <Link to="/Counselling">Counselling</Link>
-            </li>
             <li className="lg:mb-0 lg:py-0 py-3 hover:text-orangeP">
               <Link to="/login">Login</Link>
             </li>
