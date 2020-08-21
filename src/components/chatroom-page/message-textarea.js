306b49cc63524387949d662b197b8b54d7af4fa9
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-21 21:08:19 +0300

    prevents empty messages from being sent

--- src/components/chatroom-page/message-textarea.js
+++ src/components/chatroom-page/message-textarea.js
@@ -4,9 +4,26 @@ import firebase from './../../firebase/firebase';
 
 const MessageTextarea = () => {
   const messageInput = useRef('');
+
+  function createMessage() {
+    const message = messageInput.current.value.trim();
+    if (message !== '') {
+      firebase.createNewMessage(message);
+
+      messageInput.current.value = '';
+      messageInput.current.scrollIntoView();
+    } else {
+      return;
+    }
+  }
   return (
     <div className="w-11/12 text-darkP relative my-10 self-center">
       <textarea
+        onKeyDown={(key) => {
+          if (key.keyCode === 13) {
+            createMessage();
+          }
+        }}
         ref={messageInput}
         id={'text-area'}
         className="resize-none rounded-md text-darkP px-4 py-3 h-32 w-full mt-4 md:text-base text-sm"
@@ -14,9 +31,7 @@ const MessageTextarea = () => {
       ></textarea>
       <button
         onClick={() => {
-          firebase.createNewMessage(messageInput.current.value);
-          messageInput.current.value = '';
-          messageInput.current.scrollIntoView();
+          createMessage();
         }}
         className="bg-darkP flex items-center text-beige rounded-sm md:px-6 px-3 py-1 absolute bottom-0 right-0 mr-4 mb-4 hover:text-orangeP"
       >
