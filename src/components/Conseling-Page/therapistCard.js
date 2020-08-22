343daebcf189d0642d12fd5d1648920178fa63c5
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-23 00:36:39 +0300

    changes 'hourly' to 'per session'

--- src/components/Conseling-Page/therapistCard.js
+++ src/components/Conseling-Page/therapistCard.js
@@ -24,7 +24,7 @@ const TherapistCard = ({ therapist }) => {
             <span className="text-orangeP font-extrabold text-base">
               {therapist.cost}.00$
             </span>
-            &nbsp; Hourly
+            &nbsp; Per Session
           </h2>
         </div>
         <div className="flex flex-col items-start py-6 text-base md:h-full md:w-auto w-11/12 md:justify-between md:py-2">
