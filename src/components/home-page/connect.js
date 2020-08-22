460fa9e133c9ffe970e673337f2895a9b0b5af49
Yara <yaraalkassar@users.noreply.github.com>
2020-08-23 02:12:13 +0300

    fix: connect.

--- src/components/home-page/connect.js
+++ src/components/home-page/connect.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import Illustration from './image/undraw-workout.svg';
+import { Link } from 'react-router-dom';
 
 export default function Connect() {
   return (
-    <div className="w-full h-full flex justify-center items-center lg:mt-12 py-20">
-      <div className="flex xl:flex-row flex-col xl:justify-evenly justify-center items-center w-full">
-        <div className="xl:mt-0 mt-32">
-          <h1 className="text-darkP font-bold xl:text-4xl text-5xl flex-1 ">
-            Let&apos;s make a <br />
-            change today
+    <div className="flex justify-center items-center lg:mt-12 py-20">
+      <div className="flex xl:flex-row flex-col xl:justify-evenly justify-center items-center text-center">
+        <div className="xl:mt-0  leading-relaxed mx-auto md:mx-0">
+          <h1 className="text-darkP font-bold xl:text-4xl text-lg xl:w-2/3">
+            Let&apos;s make a change today
           </h1>
-          <h6 className="text-darkP xl:mt-8 mt-5 xl:text-base text-xl">
-            Connect to caring people, chat anonymously
-            <br /> and be part of a supportive community
+          <h6 className="text-darkP xl:mt-8 mt-5 xl:text-base text-xs xl:w-2/3">
+            Connect to caring people, chat anonymously and be part of a
+            supportive community
           </h6>
-          <button className="bg-darkP text-white font-bold py-2 px-4 border rounded-xl xl:mt-16 mt-10">
-            Connect Now
+          <button className="bg-darkP text-white font-bold py-2 border rounded-lg xl:mt-16 mt-10 w-1/2">
+            <Link to="/signUp" className="text-sm ml-2">
+              Connect Now
+            </Link>
           </button>
         </div>
-        <div className="flex justify-center items-center xl:my-0">
-          <img className="w-4/6" src={Illustration} />
-        </div>
       </div>
+      <img
+        className="hidden lg:block w-2/6"
+        src={Illustration}
+        alt="girl running"
+      />
     </div>
   );
 }
