6ca4ee137396b28cfc3ff55ba039e6538bb3b19b
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-22 18:08:25 +0300

    validates therapist prop in card component

--- src/components/Conseling-Page/therapistCard.js
+++ src/components/Conseling-Page/therapistCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const TherapistCard = ({ therapist }) => {
   return (
@@ -56,6 +57,10 @@ const TherapistCard = ({ therapist }) => {
       </div>
     </div>
   );
+};
+
+TherapistCard.propTypes = {
+  therapist: PropTypes.object.isRequired,
 };
 
 export default TherapistCard;
