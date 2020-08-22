118ea8c434f0d1eade80b7d33dbf63c686ba6201
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-22 21:15:33 +0300

    modifies layout and style of cards across all viewports

--- src/components/Conseling-Page/therapistCard.js
+++ src/components/Conseling-Page/therapistCard.js
@@ -3,58 +3,60 @@ import PropTypes from 'prop-types';
 
 const TherapistCard = ({ therapist }) => {
   return (
-    <div className="flex flex-col md:h-auto bg-darkP rounded-lg overflow-hidden md:flex-row md:my-24 my-16 md:p-10 p-5 mx-auto md:w-1/2 w-5/6">
-      <div className="md:flex items-center sm:w-3/4 md:h-full">
-        <div className="w-auto md:mt-0 mt-6 md:h-full flex justify-center items-center">
-          <img
-            src={therapist.profilePicture}
-            className="rounded-lg md:w-56 w-11/12 md:mx-0 mx-auto h-56 object-cover"
-            alt="Counsellor"
-          />
-        </div>
-        <div className="flex-col flex-wrap py-6 md:px-0 md:py-0 text-base md:text-sm mb-2 mx-4 md:ml-10 md:h-full">
-          <h2 className="text-orangeP text-xl">{therapist.username}</h2>
-          <div className="mb-4">
-            <h3 className="mb-2 sm:mt-3 md:mt-4 text-orangeP">Bio:</h3>
-            <h2>{therapist.therapistBio}</h2>
+    <div className="flex flex-col md:h-auto bg-darkP rounded-lg overflow-hidden md:flex-row md:my-24 my-16 md:p-10 p-5 mx-auto xl:w-1/2 lg:w-8/12 w-5/6">
+      <div className="w-auto md:mt-0 mt-6 xl:w-5/12 md:w-6/12 flex justify-center items-center">
+        <img
+          src={therapist.profilePicture}
+          className="rounded-lg md:w-56 w-11/12 md:mx-0 mx-auto md:h-48 object-cover"
+          alt="Counsellor"
+        />
+      </div>
+      <div className="flex md:flex-row flex-col justify-between items-center w-full xl:px-10 lg:pl-10 md:pl-8">
+        <div className="flex flex-col items-start py-6 text-base md:h-full md:w-auto w-11/12 md:justify-between md:py-2 md:pr-6">
+          <h2 className="text-beige text-xl md:m-0 mb-8">
+            {therapist.username}
+          </h2>
+          <div className="md:m-0 mb-8">
+            <h3 className="text-orangeP text-lg mb-1">Bio:</h3>
+            <h2 className="text-sm">{therapist.therapistBio}</h2>
           </div>
-          <h2 className="mt-4">
+          <h2 className="text-sm">
             <span className="text-orangeP font-extrabold text-base">
               {therapist.cost}.00$
-            </span>{' '}
-            Hourly
+            </span>
+            &nbsp; Hourly
           </h2>
         </div>
-      </div>
-      <div className="flex flex-col justify-between py-6 md:px-0 md:py-0 text-base md:text-sm mb-2 mx-4">
-        <div className="mt-1 sm:mt-3 md:mt-4 overflow-hidden">
-          <h2 className="text-orangeP text-xl mb-2">Specialties</h2>
-          <ul
-            className="md:overflow-y-auto md:h-20 overflow-x-hidden mb-8"
-            style={{
-              listStyleImage: 'url(srccomponentsConseling-PageForward.png)',
-            }}
-          >
-            {therapist.expertise.map((expertise, index) => {
-              return (
-                <li className="my-1" key={index}>
-                  <h2>
-                    <span className="text-orangeP mr-2">&gt;</span>
-                    {expertise}
-                  </h2>
-                </li>
-              );
-            })}
-          </ul>
+        <div className="flex flex-col items-start py-6 text-base md:h-full md:w-auto w-11/12 md:justify-between md:py-2">
+          <div className="overflow-hidden md:m-0 mb-8">
+            <h2 className="text-orangeP text-lg mb-1">Specialties:</h2>
+            <ul
+              className="md:overflow-y-auto md:h-24 overflow-x-hidden text-sm"
+              style={{
+                listStyleImage: 'url(srccomponentsConseling-PageForward.png)',
+              }}
+            >
+              {therapist.expertise.map((expertise, index) => {
+                return (
+                  <li className="my-1" key={index}>
+                    <h2>
+                      <span className="text-orangeP mr-2">&gt;</span>
+                      {expertise}
+                    </h2>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <a href="https://calendly.com/sarasaadiq98/30min">
+            <button
+              type="submit"
+              className="bg-transparent text-orangeP border border-orangeP rounded py-1 w-32"
+            >
+              Book A Time
+            </button>
+          </a>
         </div>
-        <a href="https://calendly.com/sarasaadiq98/30min">
-          <button
-            type="submit"
-            className="bg-transparent text-orangeP border border-orangeP rounded py-1 w-32"
-          >
-            Book A Time
-          </button>
-        </a>
       </div>
     </div>
   );
