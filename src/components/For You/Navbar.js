1f57256d0fdc325ea35a3781b96c69311ffaebfb
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-23 01:25:03 +0300

    refactors for-you page navbar for not-signed-in state

--- src/components/For You/Navbar.js
+++ src/components/For You/Navbar.js
@@ -14,36 +14,44 @@ export default function Navbar() {
   return (
     <>
       <nav className="relative flex flex-wrap items-center justify-between px-2 lg:py-6 py-4 navbar-expand-lg bg-beige text-darkP text-sm">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+        <div
+          className={`container px-4 mx-auto flex ${
+            currentUser ? 'flex-wrap' : ''
+          } items-center justify-between`}
+        >
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <img
               src={logo}
-              className="w-16 md:w-20 lg:ml-0 md:ml-12 ml-8 cursor-pointer"
+              className={`w-16 md:w-20 lg:ml-0 md:ml-12 ${
+                currentUser ? 'ml-8' : ''
+              } cursor-pointer`}
               alt="We Care logo"
             />
             <button
-              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              className={`text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block ${
+                currentUser ? 'lg:hidden' : 'hidden'
+              } outline-none focus:outline-none`}
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
             >
               <FiMenu className="text-2xl text-darkP" />
             </button>
           </div>
-          <div
-            className={
-              'lg:flex flex-grow items-center justify-center' +
-              (navbarOpen ? ' flex' : ' hidden')
-            }
-            id="example-navbar-danger"
-          >
-            <ul className="flex flex-col justify-center lg:flex-row list-none text-center w-full">
-              <li className="lg:mr-10 lg:my-0 lg:py-0 py-3 hover:text-orangeP lg:border-0 border-b border-darkP">
-                <Link to="/Counselling">Counselling</Link>
-              </li>
-              <li className="lg:mr-10 lg:my-0 lg:py-0 py-3 hover:text-orangeP lg:border-0 border-b border-darkP">
-                <Link to="/chatroom">Connect Now</Link>
-              </li>
-              {firebase.getCurrentUsername() ? (
+          {firebase.getCurrentUsername() ? (
+            <div
+              className={
+                'lg:flex flex-grow items-center justify-center' +
+                (navbarOpen ? ' flex' : ' hidden')
+              }
+              id="example-navbar-danger"
+            >
+              <ul className="flex flex-col justify-center lg:flex-row list-none text-center w-full">
+                <li className="lg:mr-10 lg:my-0 lg:py-0 py-3 hover:text-orangeP lg:border-0 border-b border-darkP">
+                  <Link to="/Counselling">Counselling</Link>
+                </li>
+                <li className="lg:mr-10 lg:my-0 lg:py-0 py-3 hover:text-orangeP lg:border-0 border-b border-darkP">
+                  <Link to="/chatroom">Connect Now</Link>
+                </li>
                 <li className="lg:mb-0 lg:py-0 py-3 hover:text-orangeP">
                   <Link
                     to="/login"
@@ -54,11 +62,11 @@ export default function Navbar() {
                     Sign Out
                   </Link>
                 </li>
-              ) : (
-                ''
-              )}
-            </ul>
-          </div>
+              </ul>
+            </div>
+          ) : (
+            ''
+          )}
           {firebase.getCurrentUsername() ? (
             <div className="lg:flex justify-center items-center hidden">
               <Link to="/profile">
@@ -73,12 +81,9 @@ export default function Navbar() {
               </Link>
             </div>
           ) : (
-            <Link
-              to="/login"
-              className="lg:flex justify-center items-center hidden"
-            >
-              Sign In
-            </Link>
+            <div className="flex justify-end items-center text-base w-2/4">
+              <Link to="/login">Sign In</Link>
+            </div>
           )}
         </div>
       </nav>
