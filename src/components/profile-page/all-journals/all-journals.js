177af8cad8536f56eff185f1cc4d11001255d358
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-24 13:58:41 +0300

    implements no-journals & waiting screens

--- src/components/profile-page/all-journals/all-journals.js
+++ src/components/profile-page/all-journals/all-journals.js
@@ -3,13 +3,15 @@ import { useHistory } from 'react-router-dom';
 import firebase from './../../../firebase/firebase';
 import { setCurrentJournal } from './../../../redux/actions';
 import { useDispatch } from 'react-redux';
+import noJournal from './../imgs/tree.svg';
+import Loader from './../../loader/loader';
 
 const AllJournals = () => {
   let history = useHistory();
 
   const dispatch = useDispatch();
 
-  const [journals, setJournals] = useState([]);
+  const [journals, setJournals] = useState(null);
 
   useEffect(() => {
     async function getJournals() {
@@ -35,7 +37,7 @@ const AllJournals = () => {
     getJournals();
   }, []);
 
-  return (
+  return journals ? (
     <div className="w-full overflow-y-auto h-full flex flex-col justify-center items-center">
       <div className="overflow-y-auto overflow-x-hidden w-11/12 md:w-9/12 lg:w-1/2 h-full bg-white rounded-md text-darkP flex flex-col justify-start items-center p-10 my-10">
         <div className="flex flex-col w-full justify-evenly items-center">
@@ -50,24 +52,35 @@ const AllJournals = () => {
             Add Journal
           </button>
         </div>
-        <ul className="w-full flex flex-col justify-center items-center md:text-base text-sm">
-          {journals &&
-            journals.map((journal) => (
-              <li
-                key={journal.id}
-                className="cursor-pointer border-b-2 border-grey py-2 w-full flex justify-center items-center hover:text-orangeP hover:border-orangeP"
-                onClick={(e) => {
-                  e.preventDefault();
-                  dispatch(setCurrentJournal(journal));
-                  history.push('/profile/journals/journal');
-                }}
-              >
-                {journal.title}
-              </li>
-            ))}
-        </ul>
+        {journals.length !== 0 ? (
+          <ul className="w-full flex flex-col justify-center items-center md:text-base text-sm">
+            {journals &&
+              journals.map((journal) => (
+                <li
+                  key={journal.id}
+                  className="cursor-pointer border-b-2 border-grey py-2 w-full flex justify-center items-center hover:text-orangeP hover:border-orangeP"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    dispatch(setCurrentJournal(journal));
+                    history.push('/profile/journals/journal');
+                  }}
+                >
+                  {journal.title}
+                </li>
+              ))}
+          </ul>
+        ) : (
+          <div className="h-full w-full flex flex-col justify-around items-center text-center">
+            <img className="xl:w-1/2 w-11/12 my-3" src={noJournal}></img>
+            <h3 className="sm:text-3xl text-2xl font-light">
+              You don&apos;t have any journals yet.
+            </h3>
+          </div>
+        )}
       </div>
     </div>
+  ) : (
+    <Loader />
   );
 };
 
