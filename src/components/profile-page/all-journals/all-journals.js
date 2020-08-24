5dc68d3e1588790e0351874e5a7e5791290e1b2b
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-24 09:37:45 +0300

    creates add-journal button

--- src/components/profile-page/all-journals/all-journals.js
+++ src/components/profile-page/all-journals/all-journals.js
@@ -5,7 +5,12 @@ const AllJournals = () => {
   return (
     <div className="w-full overflow-y-auto h-full flex flex-col justify-center items-center">
       <div className="overflow-y-auto w-11/12 md:w-9/12 bg-white h-full lg:w-1/2 rounded-md text-darkP flex flex-col justify-start items-center p-10 my-10">
-        <h2 className="md:text-2xl text-xl font-semibold mb-10">Journals</h2>
+        <div className="flex flex-col w-full justify-evenly items-center">
+          <h2 className="text-3xl">Journals</h2>
+          <button className="text-beige border text-center border-darkP my-8 bg-darkP h-8 border-solid rounded w-32 text-sm">
+            Add Journal
+          </button>
+        </div>
         <ul className="w-full flex flex-col justify-center items-center md:text-base text-sm">
           <li className="border-b-2 border-grey py-2 w-full flex justify-center items-center hover:text-orangeP hover:border-orangeP">
             The day she said...
