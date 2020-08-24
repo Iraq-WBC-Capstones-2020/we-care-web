1ccba8e9e7760b028f336ec9e3700aced51130f5
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-24 12:33:13 +0300

    modifies journal component

--- src/components/profile-page/profile-journal/journal.js
+++ src/components/profile-page/profile-journal/journal.js
@@ -15,9 +15,9 @@ const Journal = () => {
   return (
     currentJournal && (
       <div className="w-full h-full overflow-y-auto flex flex-col justify-center items-center">
-        <div className="overflow-y-auto bg-white rounded-md text-darkP flex flex-col justify-start items-center p-10 my-10">
-          <h2 className="text-3xl font-light mb-">{currentJournal.title}</h2>
-          <p>{currentJournal.createdAt}</p>
+        <div className="overflow-y-auto bg-white rounded-md text-darkP flex flex-col justify-start items-left py-10 px-20">
+          <h2 className="text-3xl font-light mb-4">{currentJournal.title}</h2>
+          <p className="text-lg mb-10">{currentJournal.createdAt}</p>
           <p className="text-lg">{currentJournal.body}</p>
         </div>
       </div>
