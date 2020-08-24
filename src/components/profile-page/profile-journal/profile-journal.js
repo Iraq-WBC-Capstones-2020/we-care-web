87ba599d95f0a52d97262b1260b7cbc0ebab81d0
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-24 11:32:19 +0300

    gets rid of the old code to refactor

--- src/components/profile-page/profile-journal/profile-journal.js
+++ src/components/profile-page/profile-journal/profile-journal.js
@@ -2,29 +2,9 @@ import React, { useState } from 'react';
 import Journal from './journal';
 
 const ProfileJournal = () => {
-  const [currentJournal] = useState('why does this happen to me every time?');
-  const [Journals] = useState([
-    {
-      title: 'why does this happen to me every time?',
-      body:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum, risus et iaculis vestibulum, mauris quam bibendum libero, at dignissim dui leo eget massa. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam eget dolor orci. Ut et augue sed mauris euismod ultricies et non orci. Nullam eu pulvinar enim.',
-    },
-    {
-      title: 'I got dissapointed',
-      body:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum, risus et iaculis vestibulum, mauris quam bibendum libero, at dignissim dui leo eget massa.',
-    },
-  ]);
+  const [currentJournal, setCurrentJournal] = useState(null);
 
-  return (
-    <>
-      {Journals.filter((journal) => journal.title === currentJournal).map(
-        (journal, index) => {
-          return <Journal journal={journal} key={index} />;
-        }
-      )}
-    </>
-  );
+  return <></>;
 };
 
 export default ProfileJournal;
