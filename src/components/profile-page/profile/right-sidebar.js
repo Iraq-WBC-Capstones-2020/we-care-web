713293507963b0c404f3042d8f5bfe8b40dca855
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-27 04:42:03 +0300

    counts friends in right-sidebar

--- src/components/profile-page/profile/right-sidebar.js
+++ src/components/profile-page/profile/right-sidebar.js
@@ -52,9 +52,10 @@ const RightSidebar = () => {
             </p>
           </div>
           <div className="border lg:border-beige border-darkP border-solid rounded-full py-2 mt-10 w-32 text-sm flex items-center h-10 justify-center lg:font-normal font-medium">
-            <FiSmile className="mr-3 text-xl" /> 34 friends
+            <FiSmile className="mr-3 text-xl" />({currentUser.friends.length}){' '}
+            {currentUser.friends.length === 1 ? 'friend' : 'friends'}
           </div>
-          <div className="text-orangeP  border text-center lg:border-orangeP border-darkP lg:bg-transparent mt-10 bg-darkP h-8 border-solid rounded   w-32 text-sm">
+          <div className="text-orangeP border text-center lg:border-orangeP border-darkP lg:bg-transparent mt-10 bg-darkP h-8 border-solid rounded w-32 text-sm">
             <input
               className="cursor-pointer absolute opacity-0 block  h-10  w-32 "
               type="file"
