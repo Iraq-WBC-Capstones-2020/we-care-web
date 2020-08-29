404c9222a404e0d840f3a63c244dd2a61cf35d6a
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-29 03:31:29 +0300

    handles an error in your-posts section

--- src/components/profile-page/profile-your-posts/profile-your-posts.js
+++ src/components/profile-page/profile-your-posts/profile-your-posts.js
@@ -5,7 +5,7 @@ import { putPostsIntoGrid } from '../util';
 import { useTranslation } from 'react-i18next';
 
 const ProfileYourPosts = () => {
-  const [postsList, setPosts] = useState([]);
+  const [postsList, setPosts] = useState(null);
   useEffect(() => {
     getPosts();
   }, []);
@@ -21,17 +21,21 @@ const ProfileYourPosts = () => {
   const { t } = useTranslation();
   return (
     <div className="w-full h-full flex flex-col justify-center items-center text-darkP overflow-y-auto overflow-x-hidden">
-      {postsList.length !== 0 ? (
-        <>
-          <div className="flex flex-wrap w-full justify-center items-center h-full overflow-y-auto overflow-x-hidden md:p-12 p-4 lg:p-20">
-            {putPostsIntoGrid(postsList)}
-          </div>
-        </>
+      {postsList ? (
+        postsList.length !== 0 ? (
+          <>
+            <div className="flex flex-wrap w-full justify-center items-center h-full overflow-y-auto overflow-x-hidden md:p-12 p-4 lg:p-20">
+              {putPostsIntoGrid(postsList)}
+            </div>
+          </>
+        ) : (
+          <>
+            <h1 className="text-5xl font-light mb-12">{t('No posts yet')}</h1>
+            <img src={tree} alt="tree illustration" className="w-1/4" />
+          </>
+        )
       ) : (
-        <>
-          <h1 className="text-5xl font-light mb-12">{t('No posts yet')}</h1>
-          <img src={tree} alt="tree illustration" className="w-1/4" />
-        </>
+        ''
       )}
     </div>
   );
