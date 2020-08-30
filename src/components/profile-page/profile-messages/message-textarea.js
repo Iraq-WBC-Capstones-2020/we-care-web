8d5f99126af81befe796bee3233c510ada2ff963
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-30 15:12:23 +0300

    sends message to firestore successfully

--- src/components/profile-page/profile-messages/message-textarea.js
+++ src/components/profile-page/profile-messages/message-textarea.js
@@ -1,39 +1,47 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import { FiSend } from 'react-icons/fi';
-import { useSelector, useDispatch } from 'react-redux';
-import { setMessages } from '../../../redux/actions';
+import { useSelector } from 'react-redux';
+// import { setMessages } from '../../../redux/actions';
 import { useTranslation } from 'react-i18next';
+import firebase from './../../../firebase/firebase';
 
 const MessageTextarea = () => {
-  const messagesList = useSelector((state) => state.messagesList);
-  const dispatch = useDispatch();
+  const [messageBody, setMessageBody] = useState('');
+  // const messagesList = useSelector((state) => state.messagesList);
 
-  const messageInput = useRef('');
+  const searchedUser = useSelector((state) => state.searchedUser);
+  const currentUser = useSelector((state) => state.currentUser);
+
+  // const dispatch = useDispatch();
+
   const { t } = useTranslation();
   return (
     <div className="w-5/6 lg:w-4/5 xl:w-4/6 text-darkP relative my-10 self-center">
       <h2 className="md:text-lg font-semibold">{t('Leave a message')}</h2>
       <textarea
-        ref={messageInput}
+        value={messageBody}
+        onChange={(e) => setMessageBody(e.target.value)}
         id={'message-textarea'}
         className="resize-none rounded-md text-darkP px-4 py-3 h-40 w-full mt-4 md:text-base text-sm"
         placeholder={t('start_typing')}
       ></textarea>
       <button
         onClick={() => {
-          dispatch(
-            setMessages([
-              ...messagesList,
-              {
-                from: 'Bruce Lee',
-                to: 'Harry Davies',
-                body: messageInput.current.value,
-              },
-            ])
-          );
+          firebase.sendMessage(currentUser, searchedUser, messageBody);
 
-          messageInput.current.value = '';
-          messageInput.current.scrollIntoView();
+          // dispatch(
+          //   setMessages([
+          //     ...messagesList,
+          //     {
+          //       from: 'Bruce Lee',
+          //       to: 'Harry Davies',
+          //       body: messageInput.current.value,
+          //     },
+          //   ])
+          // );
+
+          // messageInput.current.value = '';
+          // messageInput.current.scrollIntoView();
         }}
         className="bg-darkP flex items-center text-beige rounded-sm md:px-6 px-3 py-1 absolute bottom-0 right-0 mr-4 mb-4 hover:text-orangeP"
       >
