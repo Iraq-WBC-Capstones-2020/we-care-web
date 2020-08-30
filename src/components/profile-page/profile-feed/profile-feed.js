1c3c2b27ef01c0257184bbf173dbe90a44e2020d
Yara <yaraalkassar@users.noreply.github.com>
2020-08-30 22:32:24 +0300

    fixed: posts

--- src/components/profile-page/profile-feed/profile-feed.js
+++ src/components/profile-page/profile-feed/profile-feed.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Post from './post';
+import tree from '../imgs/tree.svg';
 import WritePostTextarea from './post-textarea';
 import firebase from '../../../firebase/firebase';
+import { useTranslation } from 'react-i18next';
 
 const ProfileFeed = () => {
-  const [posts, setPosts] = useState([]);
+  const { t } = useTranslation();
+  const [postsList, setPosts] = useState(null);
   useEffect(() => {
     getPosts();
   }, []);
@@ -19,12 +22,29 @@ const ProfileFeed = () => {
   }
 
   return (
-    <div className="w-full h-full flex flex-col justify-between items-center overflow-y-auto">
-      <div className="w-full flex flex-col justify-center items-center ">
-        {posts &&
-          posts.map((postObj, index) => <Post post={postObj} key={index} />)}
-      </div>
-      <WritePostTextarea posts={posts} setPosts={setPosts} />
+    <div className="w-full h-full flex flex-col justify-center items-center text-darkP overflow-y-auto overflow-x-hidden">
+      {postsList ? (
+        postsList.length !== 0 ? (
+          <>
+            <div className="w-full h-full flex flex-col justify-between items-center overflow-y-auto">
+              <div className="w-full flex flex-col justify-center items-center ">
+                {postsList &&
+                  postsList.map((postObj, index) => (
+                    <Post post={postObj} key={index} />
+                  ))}
+              </div>
+              <WritePostTextarea posts={postsList} setPosts={setPosts} />
+            </div>
+          </>
+        ) : (
+          <>
+            <h1 className="text-5xl font-light mb-12">{t('No posts yet')}</h1>
+            <img src={tree} alt="tree illustration" className="w-1/4" />
+          </>
+        )
+      ) : (
+        ''
+      )}
     </div>
   );
 };
