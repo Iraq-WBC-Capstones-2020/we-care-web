4e8acc8abcbcab2ac0d7606e7c3479eb1849847e
Yara <yaraalkassar@users.noreply.github.com>
2020-08-30 19:47:40 +0300

    add post

--- src/components/profile-page/profile-feed/post-textarea.js
+++ src/components/profile-page/profile-feed/post-textarea.js
@@ -1,14 +1,16 @@
 import React, { useRef } from 'react';
 import { FiSend } from 'react-icons/fi';
 import PropTypes from 'prop-types';
+import firebase from '../../../firebase/firebase';
+import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
-const WritePostTextarea = ({ posts, setPosts }) => {
+const WritePostTextarea = () => {
   const postInput = useRef('');
   const { t } = useTranslation();
-  return (
-    <div className="lg:w-1/2 md:w-4/5 w-11/12 text-darkP relative my-8">
-      <h2 className="md:text-lg font-semibold">{t('write_post')}</h2>
+  return firebase.getCurrentUsername() ? (
+    <div className="lg:w-1/2 md:w-4/5 w-11/12 text-orangeP relative my-8">
+      <h2 className="md:text-lg font-semibold text-darkP">{t('write_post')}</h2>
       <textarea
         ref={postInput}
         id={'post-textarea'}
@@ -17,9 +19,12 @@ const WritePostTextarea = ({ posts, setPosts }) => {
       ></textarea>
       <button
         onClick={() => {
-          setPosts([...posts, { body: postInput.current.value }]);
-          postInput.current.value = '';
-          postInput.current.scrollIntoView();
+          if (postInput.current.value !== '') {
+            addPost(postInput.current.value);
+            postInput.current.value = '';
+          } else {
+            alert('the text is empty');
+          }
         }}
         className="bg-darkP flex items-center text-beige rounded-sm md:px-6 px-3 py-1 absolute bottom-0 right-0 mr-4 mb-4 hover:text-orangeP"
       >
@@ -27,7 +32,23 @@ const WritePostTextarea = ({ posts, setPosts }) => {
         <FiSend className="ml-2" />
       </button>
     </div>
+  ) : (
+    <div>
+      <h2 className="md:text-lg text-darkBeige font-semibold mt-6 border-transparent border-b-2 hover:border-orangeP">
+        <Link to="/login" className="">
+          {t('Sign In To Write A Post')}
+        </Link>
+      </h2>
+    </div>
   );
+
+  async function addPost(body) {
+    try {
+      await firebase.createPost(body);
+    } catch {
+      alert('not working');
+    }
+  }
 };
 
 export default WritePostTextarea;
