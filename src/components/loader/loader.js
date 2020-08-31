e8e399c162b845ec5778d336c8829568407b37fc
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-08-31 10:14:26 +0300

    renders all conversations & sets a loader

--- src/components/loader/loader.js
+++ src/components/loader/loader.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './loader.css';
 
-const Loader = () => {
+const Loader = ({ classes }) => {
+  console.log(classes);
   return (
-    <div className="h-screen w-screen bg-beige flex justify-center items-center">
+    <div
+      className={`${
+        classes ? classes : 'h-screen w-screen'
+      } bg-beige flex justify-center items-center`}
+    >
       <div className="sk-chase">
         <div className="sk-chase-dot"></div>
         <div className="sk-chase-dot"></div>
--- src/components/profile-page/all-messages/all-messages.js
+++ src/components/profile-page/all-messages/all-messages.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import firebase from './../../../firebase/firebase';
+import Loader from './../../loader/loader';
 
 const AllMessages = () => {
   const [usersDocs, setUsersDocs] = useState(null);
@@ -16,19 +17,26 @@ const AllMessages = () => {
 
   console.log(usersDocs);
 
-  return (
+  return usersDocs ? (
     <div className="w-full h-full flex flex-col justify-center items-center">
       <div className="overflow-y-auto w-11/12 md:w-9/12 bg-white h-full lg:w-1/2 rounded-md text-darkP flex flex-col justify-start items-center p-10 my-10">
         <h2 className="md:text-2xl text-xl font-semibold mb-10">
           {t('Messages')}
         </h2>
         <ul className="w-full flex flex-col justify-center items-center md:text-base text-sm">
-          <li className="border-b-2 border-grey py-2 w-full flex justify-center items-center hover:text-orangeP hover:border-orangeP">
-            Leena Elliana
-          </li>
+          {usersDocs.map((user) => (
+            <li
+              key={user.uid}
+              className="border-b-2 border-grey py-2 w-full flex justify-center items-center hover:text-orangeP hover:border-orangeP"
+            >
+              {user.username}
+            </li>
+          ))}
         </ul>
       </div>
     </div>
+  ) : (
+    <Loader classes={'w-full h-full'} />
   );
 };
 
